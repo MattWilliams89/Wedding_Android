fd7e4d574cce6d96ee39f1bc5f8de6cb76e3641c
Matt Williams <matthew.williams01@bbc.co.uk>
2016-05-02 14:01:15 +0100

    Fixing string issue

--- pipeline.groovy
+++ pipeline.groovy
@@ -26,7 +26,7 @@ branches["releaseBuild"] = {
         cleanUnstash('sources')
         buildRelease()
         sh 'cp app/build/outputs/apk/app-release.apk wedding_app_' + "${env.BUILD_NUMBER}" + '.apk'
-        archive "${env.BUILD_NUMBER}" + '.apk'
+        archive ''+ "${env.BUILD_NUMBER}" + '.apk'
     }
 }
 
@@ -57,12 +57,15 @@ private void buildRelease() {
 }
 
 private void uploadToHockey() {
-    unarchive mapping:['*.apk' : '.']
+
+    def apkName = '' + "${env.BUILD_NUMBER}" + '.apk'
+
+    unarchive mapping:[apkName : '.']
 
     sh 'curl \\' +
             '  -F "status=2" \\' +
             '  -F "notify=1" \\' +
-            '  -F "ipa=@wedding_app_"'+"${env.BUILD_NUMBER}"+'".apk" \\' +
+            '  -F "ipa=@wedding_app_"'+ apkName +' \\' +
             '  -H "X-HockeyAppToken: "' + HOCKEY_API_KEY + ' \\' +
             '  https://rink.hockeyapp.net/api/2/apps/6a61f0dc39884d0c969068bac05a1af9/app_versions/upload'
 }
